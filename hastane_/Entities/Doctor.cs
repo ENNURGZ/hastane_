0bf5d4c8bd28a3b3bec0a75883115158670bd996
asliyilmz <asli.yilmazz.4125@gmail.com>
2023-12-14 14:16:41 +0300

    kayıt

---
+++ hastane_/Entities/Doctor.cs
@@ -0,0 +1,40 @@
+﻿/*using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace hastane_.Entities
+{
+    [Table("Doctors")]
+    public class Doctor
+    {
+        [Key]
+        public Guid DoctorId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Surname { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+
+        public bool Locked { get; set; } = false;
+
+        [Required]
+        [StringLength(50)]
+        public string Role { get; set; } = "doctor";
+
+        [StringLength(50)]
+        public string? Poliklinik { get; set; } = null;
+    
+        public ICollection<Randevu> Randevular { get; set; }
+    }
+}
+*/
