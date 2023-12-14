35db3fb4c5f27a4dbbc144a6e28d13c6ff83395e
ENNURGZ <ennurgz002@gmail.com>
2023-12-14 12:37:01 +0300

    çekmeden önce işle

--- hastane_/Models/RegisterViewModel.cs
+++ hastane_/Models/RegisterViewModel.cs
@@ -5,19 +5,19 @@ namespace hastane_.Models
     public class RegisterViewModel
     {
         //[Display(Name ="Kullanıcı Adı", Prompt ="johndoe")]
-        [Required(ErrorMessage = "Username is required.")]
+        [Required(ErrorMessage = "Kullanıcı Adı Geçersiz.")]
         [StringLength(30, ErrorMessage = "Username can be max 30 characters.")]
         public string Username { get; set; }
 
         //[DataType(DataType.Password)]
-        [Required(ErrorMessage = "Password is required.")]
-        [MinLength(6, ErrorMessage = "Password can be min 6 characters.")]
-        [MaxLength(16, ErrorMessage = "Password can be max 16 characters.")]
+        [Required(ErrorMessage = "Şifre Eşleşmiyor.")]
+        [MinLength(6, ErrorMessage = "Şifre En Az 6 Karakter Olmalıdır.")]
+        [MaxLength(16, ErrorMessage = "Şifre En Fazla 16 Karakter Olabilir.")]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Re-Password is required.")]
-        [MinLength(6, ErrorMessage = "Re-Password can be min 6 characters.")]
-        [MaxLength(16, ErrorMessage = "Re-Password can be max 16 characters.")]
+        [Required(ErrorMessage = "Şifre Eşleşmiyor.")]
+        [MinLength(6, ErrorMessage = "Şifre En Az 6 Karakter Olmalıdır.")]
+        [MaxLength(16, ErrorMessage = "Şifre En Fazla 16 Karakter Olabilir.")]
         [Compare(nameof(Password))]
         public string RePassword { get; set; }
     }
